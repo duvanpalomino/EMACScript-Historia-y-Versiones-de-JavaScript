cb997b2bd786d5eb76d70b67d989cc68c11b0c41
duvanpalomino <duvanpalomino@gmail.com>
2023-02-23 11:22:15 -0500

    Spread operator

--- src/es6/05-rest-spread.js
+++ src/es6/05-rest-spread.js
@@ -12,3 +12,24 @@ let user = {
 };
 let {username, age} = user;
 console.log(username, user.age);
+
+// spread operator
+
+let person = { 
+    name: 'Oscar',
+    age: 28,
+};
+let country = 'MX';
+
+let data = { id: 1, ...person, country};
+console.log(data);
+
+// rest
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+};
+
+sum(1, 1, 2, 3);
