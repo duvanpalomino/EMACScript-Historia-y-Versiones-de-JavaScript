b990ee39dd0e8cf387299a0b1403116551a79695
duvanpalomino <duvanpalomino@gmail.com>
2023-02-24 15:45:14 -0500

    'optional chaining'

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,29 @@
+const users ={
+    duvanpalomino: {
+        country: 'Colombia'
+    },
+    ana: {
+        country: 'Mexico'
+    },
+};
+  
+console.log(users?.bebeloper?.country);
+
+// const usuario = {}
+// console.log(usuario.redes)
+
+// const usuario = {}
+// console.log(usuario.redes.facebook)
+
+// const usuario = {}
+// console.log(usuario.redes?.facebook) 
+
+// const users1 = {
+//     gndx: {
+//         country: "MX"
+//     },
+//     ana: {
+//         country: "CO"
+//     }
+// }
+// console.log(users1?.bebeloper?.country);
